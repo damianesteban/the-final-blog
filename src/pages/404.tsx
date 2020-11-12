c99b2fd40d61de0911b0071bf6ac3c1f0cb91850
Damian Esteban <damian.esteban@gmail.com>
2020-11-11 20:48:40 -0500

    Add another fake window

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import { window, document } from "browser-monads";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
